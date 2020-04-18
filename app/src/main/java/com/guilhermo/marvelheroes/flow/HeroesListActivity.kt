7b3734efa489852b637f31bab2a07095cd668703
Guilherme <guilhermeasrgl9@gmail.com>
2020-04-18 11:47:27 -0300

    Added main toolbar XML

--- app/src/main/java/com/guilhermo/marvelheroes/flow/HeroesListActivity.kt
+++ app/src/main/java/com/guilhermo/marvelheroes/flow/HeroesListActivity.kt
@@ -1,12 +1,13 @@
-package com.guilhermo.marvelheroes
+package com.guilhermo.marvelheroes.flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.guilhermo.marvelheroes.R
 
-class MainActivity : AppCompatActivity() {
+class HeroesListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_heroes_list)
     }
 }
